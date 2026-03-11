a577138872f72120e689bc27fa74ba9b5c48a32c
thefuturecoder03 <s2140775@students.ncl-coll.ac.uk>
2026-03-11 13:19:19 +0000

    removed 3 lines

--- index.php
+++ index.php
@@ -1,8 +1,2 @@
 <marquee><h1>Hello from Docker with tiny update and a cool animation</h1></marquee>
 
-<marquee><h1>Hello from Docker with tiny update and a cool animation</h1></marquee>
-
-<marquee><h1>Hello from Docker with tiny update and a cool animation</h1></marquee>
-
-<marquee><h1>Hello from Docker with tiny update and a cool animation</h1></marquee>
-
